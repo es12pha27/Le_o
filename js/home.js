558ce514125aa2d7158e455e5cf3adbf2ce8dbd0
Estephany <esurcoa@unsa.edu.pe>
2020-01-22 16:50:32 -0500

    La primer version de la app

---
+++ js/home.js
@@ -0,0 +1,33 @@
+function pagPaginas() {
+    document.getElementById("importante").src="paginas.html";
+   }
+   function pagCapitulos() {
+    document.getElementById("importante").src="capitulos.html";
+   }
+
+   function pagDigital() {
+    document.getElementById("importante").src="digital.html";
+   }
+   function pagFisico() {
+    document.getElementById("importante").src="fisicos.html";
+   }
+  function iconoMenuMasHorario() {
+    document.getElementById("horario").style.display = "block";
+    document.getElementById("mas").style.display = "none";
+    document.getElementById("menos").style.display = "inline";
+  }
+  function iconoMenuMenosHorario() {
+    document.getElementById("horario").style.display = "none";
+    document.getElementById("mas").style.display = "inline";
+    document.getElementById("menos").style.display = "none";
+  }
+  function iconoMenuMasCompra() {
+    document.getElementById("compra").style.display = "block";
+    document.getElementById("mas1").style.display = "none";
+    document.getElementById("menos1").style.display = "inline";
+  }
+  function iconoMenuMenosCompra() {
+    document.getElementById("compra").style.display = "none";
+    document.getElementById("mas1").style.display = "inline";
+    document.getElementById("menos1").style.display = "none";
+  }
